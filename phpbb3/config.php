71eee993b3005c642a33adb387b6d811fcfb1d88
chenzhong <chenzhong@iscas.ac.cn>
2016-05-31 14:28:38 +0800

    config

---
+++ phpbb3/config.php
@@ -0,0 +1,17 @@
+<?php
+// phpBB 3.1.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'phpbb\\db\\driver\\mysql';
+$dbhost = '10.10.26.58';
+$dbport = '3306';
+$dbname = 'Hf0T8IciAbxNGFW4';
+$dbuser = 'uNdOPf21xquBvgRV';
+$dbpasswd = 'pniCqAo0cYwIsSvWr';
+$table_prefix = 'phpbb_';
+$phpbb_adm_relative_path = 'adm/';
+$acm_type = 'phpbb\\cache\\driver\\file';
+
+@define('PHPBB_INSTALLED', true);
+// @define('PHPBB_DISPLAY_LOAD_TIME', true);
+// @define('DEBUG', true);
+// @define('DEBUG_CONTAINER', true);
